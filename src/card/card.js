a61e52998119847a5f1c5f9dedc959a99ccf712a
plastya-flomaster <nastyapl98@gmail.com>
2019-08-08 04:37:45 +0500

    webpack

--- src/card/card.js
+++ src/card/card.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import './card.css';
 
-const Card = (props) => {
+class Card extends React.Component {
+constructor(props){
+    super(props);
+
+    this.state = {
+        isMouseOver: false,
+        isClicked: false
+    }
+  };
+
+  onChangeCard()
+  {
+      this.setState(
+         { isClicked: !this.state.isClicked}
+      )
+  }
+  onMouseOver()
+  {
+      this.setState(
+          {isHovered: !this.state.isMouseOver}         
+      )
+  }
+  
+render(){
+    //не очень поняла, как это работает
+    let self = this;
+    function changeCard()
+    {
+        if (self.state.isClicked)
+        {
+ return 'card-body-alternate';
+        }
+        else return 'card-body';
+    }
     return (
-        <div className='card-body'>
-            <img className='image' src={props.image} alt={props.description}></img>
-            <h1>{props.category}</h1>
-            <h2>{props.text}</h2>
+        <div className={changeCard()} onClick={this.onChangeCard.bind(this)} onMouseOver={this.onMouseOver.bind(this)}>
+            <img className='image' src={this.props.image} alt={this.props.description}></img>
+            <h1>{this.props.category}</h1>
+            <h2>{this.props.text}</h2>
         </div>
     )
 };
+}
+    
 Card.defaultProps = {
     description: 'картинка',
     category: 'название рубрики',
